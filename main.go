8bdf79f91bb9d7b22538ed1d944647aa101762b6
nixpig <143995476+nixpig@users.noreply.github.com>
2024-08-08 09:12:51 +0100

    chore: a bit of a naive solution

--- main.go
+++ main.go
@@ -1,88 +1,114 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"os"
 	"strings"
-	"unicode/utf8"
+
+	"github.com/spf13/pflag"
 )
 
 type config struct {
-	c bool
-	l bool
-	w bool
-	m bool
-	t bool
+	file  string
+	c     bool
+	l     bool
+	w     bool
+	m     bool
+	total string
+	L     bool
 }
 
 func main() {
+	c := pflag.BoolP("bytes", "c", false, "print the byte counts")
+	l := pflag.BoolP("lines", "l", false, "print the newline counts")
+	w := pflag.BoolP("words", "w", false, "print the word counts")
+	m := pflag.BoolP("chars", "m", false, "print the character counts")
+	t := pflag.String("total", "auto", "when to print a line with total counts; WHEN can be: auto, always, only, never")
+	L := pflag.BoolP("max-line-length", "L", false, "print the maximum display width")
 
-	c := flag.Bool("c", false, "print the byte counts")
-	l := flag.Bool("l", false, "print the newline counts")
-	w := flag.Bool("w", false, "print the word counts")
-	m := flag.Bool("m", false, "print the character counts")
-	t := flag.Bool("total", false, "print the total")
-
-	flag.Parse()
+	pflag.Parse()
 
 	cfg := config{
-		c: *c,
-		l: *l,
-		w: *w,
-		m: *m,
-		t: *t,
+		c:     *c,
+		l:     *l,
+		w:     *w,
+		m:     *m,
+		total: *t,
+		L:     *L,
 	}
 
-	if len(flag.Args()) == 0 {
+	var counted []count
+
+	if len(pflag.Args()) == 0 {
 		contents, err := io.ReadAll(os.Stdin)
 		if err != nil {
 			fmt.Printf("error reading from stdin: %s", err)
 			os.Exit(1)
 		}
 
-		fmt.Println(count(string(contents), cfg))
-		return
+		counted = append(counted, wc(string(contents), cfg))
+	} else {
+		files := pflag.Args()
+		counted = make([]count, len(files))
+
+		for i, file := range files {
+			contents, err := os.ReadFile(file)
+			if err != nil {
+				fmt.Printf("error reading file '%s': %s\n", file, err)
+				os.Exit(1)
+			}
+
+			cfg.file = file
+
+			counted[i] = wc(string(contents), cfg)
+		}
 	}
 
-	for _, file := range flag.Args() {
-		contents, err := os.ReadFile(file)
-		if err != nil {
-			fmt.Printf("error reading file '%s': %s\n", file, err)
-			os.Exit(1)
+	switch cfg.total {
+	case "always":
+		counted = append(counted, total(counted))
+
+	case "only":
+		counted = []count{total(counted)}
+
+	case "never":
+		break
+
+	case "auto":
+		fallthrough
+	default:
+		if len(counted) > 1 {
+			counted = append(counted, total(counted))
 		}
 
-		fmt.Println(count(string(contents), cfg))
-	}
-}
-
-func count(contents string, cfg config) string {
-	counted := strings.Builder{}
-
-	if !cfg.c && !cfg.l && !cfg.w && !cfg.m {
-		cfg.c = true
-		cfg.l = true
-		cfg.w = true
 	}
 
-	if cfg.l {
-		counted.WriteString(fmt.Sprintf("%d ", len(strings.Split(string(contents), "\n"))))
+	for _, line := range counted {
+		b := strings.Builder{}
+
+		if cfg.c {
+			b.WriteString(fmt.Sprintf("%d ", line.c))
+		}
+
+		if cfg.l {
+			b.WriteString(fmt.Sprintf("%d ", line.l))
+		}
+
+		if cfg.w {
+			b.WriteString(fmt.Sprintf("%d ", line.w))
+		}
+
+		if cfg.m {
+			b.WriteString(fmt.Sprintf("%d ", line.m))
+		}
+
+		if cfg.L {
+			b.WriteString(fmt.Sprintf("%d ", line.L))
+		}
+
+		b.WriteString(fmt.Sprintf("%s", line.file))
+
+		fmt.Println(b.String())
 	}
-
-	if cfg.w {
-		counted.WriteString(fmt.Sprintf("%d ", len(strings.Fields(string(contents)))))
-	}
-
-	if cfg.c {
-		counted.WriteString(fmt.Sprintf("%d ", len(contents)))
-	}
-
-	if cfg.m {
-		counted.WriteString(fmt.Sprintf("%d ", utf8.RuneCount([]byte(contents))))
-	}
-
-	counted.WriteString("\n")
-
-	return counted.String()
 }
